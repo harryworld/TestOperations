ca7ead67d9f2b6f042b0afb134db0ded6911d9d9
Harry Ng <harryworld@gmail.com>
2016-09-10 10:04:44 +0800

    Initial Commit.

---
+++ TestOperations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestOperations
+//
+//  Created by Harry Ng on 10/9/2016.
+//  Copyright © 2016 StaySorted Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
